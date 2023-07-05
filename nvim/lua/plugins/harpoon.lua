5843245417a9c448e789a061c37433c40febe2aa
Vladimir Hooke <g@hooke.dev>
2023-07-05 12:23:39 +0200

    nvim: harpoon keymaps

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -10,12 +10,12 @@ if not status then
 end
 
 
-vim.keymap.set("n", "<leader>aa", mark.add_file, {desc = "add file to harpoon"})
-vim.keymap.set("n", "<leader>ap", ui.toggle_quick_menu, {desc = "toggle harpoon menu"})
+vim.keymap.set("n", "<leader>a", mark.add_file, {desc = "add file to harpoon"})
+vim.keymap.set("n", "<leader>p", ui.toggle_quick_menu, {desc = "toggle harpoon menu"})
 
-vim.keymap.set("n", "<leader>6", function() ui.nav_file(1) end, {desc = "navigate to file 1"})
-vim.keymap.set("n", "<leader>7", function() ui.nav_file(2) end, {desc = "navigate to file 2"})
-vim.keymap.set("n", "<leader>8", function() ui.nav_file(3) end, {desc = "navigate to file 3"})
-vim.keymap.set("n", "<leader>9", function() ui.nav_file(4) end, {desc = "navigate to file 4"})
-vim.keymap.set("n", "<leader>0", function() ui.nav_file(5) end, {desc = "navigate to file 5"})
+vim.keymap.set("n", "<leader>:", function() ui.nav_file(1) end, {desc = "navigate to file 1"})
+vim.keymap.set("n", "<leader>?", function() ui.nav_file(2) end, {desc = "navigate to file 2"})
+vim.keymap.set("n", "<leader>*", function() ui.nav_file(3) end, {desc = "navigate to file 3"})
+vim.keymap.set("n", "<leader>(", function() ui.nav_file(4) end, {desc = "navigate to file 4"})
+vim.keymap.set("n", "<leader>)", function() ui.nav_file(5) end, {desc = "navigate to file 5"})
 
