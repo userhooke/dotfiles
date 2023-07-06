881b8f02e011674e443adaf4e03e5785b097ab8a
Vladimir Hooke <g@hooke.dev>
2023-07-06 15:30:39 +0200

    nvim: cmp keymaps

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,6 +9,11 @@ if not status then
   return
 end
 
+local status, cmp = pcall(require, "cmp")
+if not status then
+  return
+end
+
 lsp.preset("recommended")
 
 lsp.ensure_installed({
@@ -22,6 +27,19 @@ lsp.ensure_installed({
   "makefile",
   "dockerfile",
   "bashls",
+})
+
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ["<CR>"] = cmp.mapping.confirm({ select = false }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings["<Tab>"] = nil
+cmp_mappings["<S-Tab>"] = nil
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
 })
 
 lsp.on_attach(function(client, bufnr)
